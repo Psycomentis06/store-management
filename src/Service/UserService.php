00a08672efd71b286d5a48abaef673e845060e84
Psycomentis06 <alibenamor30@gmail.com>
2022-04-16 11:21:42 +0100

    Getting started

---
+++ src/Service/UserService.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Service;
+
+use App\Entity\User;
+
+class UserService
+{
+    public function __construct()
+    {
+    }
+
+    public function login(string $username, string $password)
+    {
+
+    }
+
+    public function logout()
+    {
+
+    }
+
+    public function create(User $user)
+    {
+
+    }
+
+}
