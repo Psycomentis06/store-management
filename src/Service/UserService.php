f5e3a412efd850d9f442bce4603dff563c6be620
Psycomentis06 <alibenamor30@gmail.com>
2022-04-21 18:31:30 +0100

    Reset password methods header

--- src/Service/UserService.php
+++ src/Service/UserService.php
@@ -5,6 +5,9 @@ namespace App\Service;
 use App\Entity\User;
 use App\Exception\UserAlreadyExistsException;
 use Doctrine\Persistence\ManagerRegistry;
+use Redis;
+use Symfony\Component\HttpKernel\Kernel;
+use Symfony\Component\HttpKernel\KernelInterface;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Security\Core\Exception\UserNotFoundException;
 
@@ -12,8 +15,10 @@ class UserService
 {
     private UserPasswordHasherInterface $userPasswordHasher;
     private ManagerRegistry $managerRegistry;
-    public function __construct(UserPasswordHasherInterface $userPasswordHasher, ManagerRegistry $managerRegistry)
+    private Redis $redis;
+    public function __construct(UserPasswordHasherInterface $userPasswordHasher, ManagerRegistry $managerRegistry, Redis $redis)
     {
+        $this->redis = $redis;
         $this->managerRegistry = $managerRegistry;
         $this->userPasswordHasher = $userPasswordHasher;
     }
@@ -55,4 +60,30 @@ class UserService
         return $user;
     }
 
+    /**
+     * Send a temp verification key to the user's email
+     * @return void
+     */
+    public function requestResetPassword(User $user)
+    {
+
+    }
+
+    /**
+     * Check user's verification key
+     * @return void
+     */
+    public function verifyResetPassword()
+    {
+
+    }
+
+    /**
+     * Reset user's password
+     * @return void
+     */
+    public function resetPassword()
+    {
+
+    }
 }
