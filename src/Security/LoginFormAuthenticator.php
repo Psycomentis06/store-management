29a436df9bec8bfc842c3f270dfe5519fe96c080
Psycomentis06 <alibenamor30@gmail.com>
2022-04-25 02:09:48 +0100

    Basic login steps

--- src/Security/LoginFormAuthenticator.php
+++ src/Security/LoginFormAuthenticator.php
@@ -2,23 +2,29 @@
 
 namespace App\Security;
 
+use App\Exception\AuthenticationException;
 use App\Repository\UserRepository;
+use App\Utils\RedisKeys;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Symfony\Component\Security\Core\User\UserInterface;
 use Symfony\Component\Security\Http\Authenticator\AbstractLoginFormAuthenticator;
 use Symfony\Component\Security\Http\Authenticator\Passport\Badge\CsrfTokenBadge;
 use Symfony\Component\Security\Http\Authenticator\Passport\Badge\UserBadge;
+use Symfony\Component\Security\Http\Authenticator\Passport\Credentials\CustomCredentials;
 use Symfony\Component\Security\Http\Authenticator\Passport\Credentials\PasswordCredentials;
 use Symfony\Component\Security\Http\Authenticator\Passport\Passport;
-use function dd;
 
 class LoginFormAuthenticator extends AbstractLoginFormAuthenticator
 {
     private UserRepository $userRepository;
-    public function __construct(UserRepository $userRepository)
+    private \Redis $redis;
+
+    public function __construct(UserRepository $userRepository, \Redis $redis)
     {
         $this->userRepository = $userRepository;
+        $this->redis = $redis;
     }
 
     public function authenticate(Request $request): Passport
@@ -29,16 +35,39 @@ class LoginFormAuthenticator extends AbstractLoginFormAuthenticator
         $csrfToken = $request->request->get('_csrf_token');
 
         return new Passport(
-            new UserBadge($login, function ($userIdentifier) {
-                return $this->userRepository->findOneByIdOrUsernameOrEmail($userIdentifier);
-            }),
-            new PasswordCredentials($password),
-            [new CsrfTokenBadge('login', $csrfToken)]
+            new UserBadge($login),
+            new CustomCredentials(function ($credentials, UserInterface $user) use ($request) {
+                // 1- Check password
+                $passwordPassport = new PasswordCredentials($credentials);
+                if (!$passwordPassport->isResolved()) throw new AuthenticationException("Wrong Password", AuthenticationException::WRONG_PASSWORD);
+
+                // 2- Check if there is session already
+                if (!empty($this->redis->get(RedisKeys::getSessionId($user->getUserIdentifier())))) throw new AuthenticationException('User Already logged in from other device', AuthenticationException::SINGLE_SESSION);
+
+                // 3- Check if account locked
+                $lockedRedisKey = RedisKeys::getLoginLocked($user->getUserIdentifier());
+                $locked = $this->redis->get($lockedRedisKey);
+                $lockedRemainDuration = 0;
+
+                if (!empty($locked)) {
+                    $lockedRemainDuration = $this->redis->getTimeout($lockedRedisKey);
+                    throw new AuthenticationException("Account locked for $lockedRemainDuration seconds due to many failed login attempts");
+                }
+
+                // Set session last ID
+                $request->request->set('userId', $user->getUserIdentifier());
+
+                return true;
+            }, $password),
+            [
+                new CsrfTokenBadge('login', $csrfToken)
+            ]
         );
     }
 
     public function onAuthenticationSuccess(Request $request, TokenInterface $token, string $firewallName): ?Response
     {
+        $this->redis->set(RedisKeys::getSessionId($request->request->get('userId')), $request->getSession()->getId());
         return null;
     }
 
