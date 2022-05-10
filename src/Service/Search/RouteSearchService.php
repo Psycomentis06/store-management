c0fd41d797438c4cce0140433d90d97fef0ea30e
Psycomentis06 <alibenamor30@gmail.com>
2022-05-10 20:10:17 +0100

    findAllByNameLike associative array

--- src/Service/Search/RouteSearchService.php
+++ src/Service/Search/RouteSearchService.php
@@ -2,7 +2,6 @@
 
 namespace App\Service\Search;
 
-use Symfony\Component\Routing\RouteCollection;
 use Symfony\Component\Routing\RouterInterface;
 
 class RouteSearchService
@@ -14,13 +13,19 @@ class RouteSearchService
         $this->router = $router;
     }
 
-    public function findAllByNameLike(string $name): array
+    public function findAllByNameLike(string $name = "", bool $associative = true): array
     {
-        $res = [];
         $routes = $this->router->getRouteCollection()->all();
-        foreach ($routes as $routeName => $route) {
-            if (preg_match("($name)", $routeName)) $res[] = $route;
+        if ($associative) {
+            return array_filter($routes, function ($key) use ($name) {
+                return preg_match("($name)", $key);
+            }, ARRAY_FILTER_USE_KEY);
+        } else {
+            $res = [];
+            foreach ($routes as $routeName => $route) {
+                if (preg_match("($name)", $routeName)) $res[] = $route;
+            }
+            return $res;
         }
-        return $res;
     }
 }
