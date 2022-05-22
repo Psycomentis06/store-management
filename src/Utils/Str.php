89aca56a6c8fa76e377cdc63ce7531c3b913509d
Psycomentis06 <alibenamor30@gmail.com>
2022-05-22 18:31:04 +0100

    transform str from and to underscore|Capital

--- src/Utils/Str.php
+++ src/Utils/Str.php
@@ -13,4 +13,21 @@ class Str
     {
         return preg_match('',$str);
     }
+
+    public static function underscoresToCapital(string $str): string
+    {
+        $ar = explode('_', $str);
+        $res = "";
+        foreach ($ar as $item) {
+            $res .= ucfirst($item);
+        }
+
+        return $res;
+    }
+
+    public static function capitalToUnderscore(string $str): string
+    {
+        $ar = preg_split('/(?=[A-Z])/', $str, -1, PREG_SPLIT_NO_EMPTY);
+        return strtolower(join('_', $ar));
+    }
 }
