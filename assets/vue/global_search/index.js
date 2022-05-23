485bc21f26957b077c98cd982a2f233f5a951120
Psycomentis06 <alibenamor30@gmail.com>
2022-05-23 17:32:56 +0100

    use pinia

--- assets/vue/global_search/index.js
+++ assets/vue/global_search/index.js
@@ -1,6 +1,8 @@
 import { createApp } from 'vue';
+import { createPinia } from 'pinia';
 import App from './App';
 
 
 const app = createApp(App)
+app.use(createPinia())
 app.mount('#global-search-vue')
