e22b01c2ee5f5c25ddac2b264bf910fefe6368dd
Psycomentis06 <alibenamor30@gmail.com>
2022-05-26 17:03:24 +0100

    roles bug fix

--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -2,22 +2,53 @@
 
 namespace App\Form;
 
+use App\Entity\Role;
 use App\Entity\User;
+use App\Repository\RoleRepository;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\CallbackTransformer;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class UserType extends AbstractType
 {
+    private RoleRepository $roleRepository;
+
+    public function __construct(RoleRepository $roleRepository)
+    {
+        $this->roleRepository = $roleRepository;
+    }
+
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
             ->add('username')
-            ->add('password')
+            ->add('password', PasswordType::class)
             ->add('email')
-            ->add('roles')
-            ->add('metadata')
-            ->add('state');
+            ->add('roles', EntityType::class, [
+                'class' => Role::class,
+                'expanded' => true,
+                'multiple' => true,
+                'choice_label' => 'role',
+            ])
+            ->get('roles')
+            ->addModelTransformer(
+                new CallbackTransformer(
+                    function ($roles) {
+                        $res = [];
+                        foreach ($roles as $roleName) {
+                            $role = $this->roleRepository->findOneBy(['role' => $roleName]);
+                            if (!empty($role))
+                                $res[] = $role;
+                        }
+                        return $res;
+                    },
+                    function ($roles) {
+                        return $roles;
+                    })
+            );
     }
 
     public function configureOptions(OptionsResolver $resolver): void
