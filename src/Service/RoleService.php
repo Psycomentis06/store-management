4dea81f1b4353444655565f6d9cfdfe59f91a003
Psycomentis06 <alibenamor30@gmail.com>
2022-05-26 14:34:46 +0100

    addDefaultPermissions

---
+++ src/Service/RoleService.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Service;
+
+use App\Entity\Role;
+use App\Repository\PermissionRepository;
+use App\Repository\RoleRepository;
+
+class RoleService
+{
+
+    private RoleRepository $roleRepository;
+    private PermissionRepository $permissionRepository;
+
+    public function __construct(RoleRepository $roleRepository, PermissionRepository $permissionRepository)
+    {
+        $this->roleRepository = $roleRepository;
+        $this->permissionRepository = $permissionRepository;
+    }
+
+    public function addDefaultPermissions()
+    {
+        $roles = ['ROLE_USER' => 'user', 'ROLE_SUPERADMIN' => 'superadmin'];
+        foreach ($roles as $roleName => $roleDefaultName) {
+            $roleEntity = $this->roleRepository->findOneBy(['role' => $roleName]);
+            $permissions = $this->permissionRepository->findByDefaultRole($roleDefaultName);
+            if (!empty($roleEntity) && $roleEntity instanceof Role) {
+                foreach ($permissions as $permission) {
+                    $roleEntity->addPermission($permission);
+                }
+                $this->roleRepository->add($roleEntity);
+            }
+        }
+    }
+
+    public function cloneRole(string $from, string $newRoleName)
+    {
+
+    }
+
+}
